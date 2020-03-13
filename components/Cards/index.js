f05a01b1447d4fc8fa961bb878949efebc85a989
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-03-13 13:03:29 -0500

    finished everything but the cards component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,83 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+// for (const [key, value] of Object.entries(object.data.articles)) {
+//     console.log(`key is => ${key}, value is => ${value.forEach(item => {console.log(item.authorName)})}`); 
+// }
+
+// let obj = object.data.articles; 
+    
+// for (const [key, value] of Object.entries(obj)) {
+//     console.log(`key is ${key} value is ${value.forEach(item => {
+//         console.log(item.headline)
+//     })}`)
+// }
+
+function cardsCreator (object) {
+
+    const newArr = object; 
+    let arr = Object.keys(newArr.data.articles);
+
+    const card = document.createElement('div'), 
+          headLine = document.createElement('div'), 
+          author = document.createElement('div'), 
+          imgContainer = document.createElement('div'),
+          imgSrc = document.createElement('img'), 
+          authorsName = document.createElement('span'); 
+
+
+    card.classList.add('card'); 
+    headLine.classList.add('headline'); 
+    author.classList.add('author'); 
+    imgContainer.classList.add('img-container'); 
+    
+    headLine.textContent = arr.map((key) => {
+        return {[key]: newArr.data.articles[key].forEach(item => {return item.headline})};
+        }); 
+    
+    imgSrc.src = arr.map((key) => {
+        return {[key]: newArr.data.articles[key].forEach(item => { return item.authorPhoto})};
+        }); 
+
+    authorsName.textContent = arr.map((key) => {
+        return {[key]: newArr.data.articles[key].forEach(item => { return item.authorName})};
+        });
+
+    card.append(headLine, author); 
+    author.append(imgContainer, authorsName); 
+    imgContainer.append(imgSrc); 
+
+    return card; 
+
+}
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+
+    const cardParent = document.querySelector('.cards-container'); 
+
+    cardParent.append(cardsCreator(response)); 
+    // newArr = response; 
+    // let arr = Object.keys(newArr.data.articles);
+    // console.log(arr); 
+    // let arrObj = arr.map((key) => {
+    // return {[key]: newArr.data.articles[key].forEach(item => {console.log(item.headline)})};
+    // })
+    console.log(response); 
+    // console.log(arrObj);
+    // console.log(response); 
+    // console.log(cardsCreator(response));
+}); 
+
+// newArr = response; 
+// let arr = Object.keys(newArr.data.articles);
+// console.log(arr); 
+// let arrObj = arr.map((key) => {
+   // return {[key]: newArr.data.articles[key].forEach(item => {console.log(item.headline)})};
+// })
+// console.log(response); 
+// console.log(arrObj);
+// console.log(response); 
+// console.log(cardsCreator(response));
